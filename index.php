f842dad286ac628cd12d7b959ccdd0821db56abf
unknown <sharmaprishita10@gmail.com>
2022-10-04 15:21:02 +0530

    Add Orissa NGO table

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-$servername="localhost";
-$username="root";
-$password="";
-$database="pravasi_ngo_db";
-$conn=mysqli_connect($servername,$username,$password,$database);
-if(!$conn)
-    die("Sorry failed to connect : ",mysqli_connect_error());
-else
-    echo("Connection was successful");
+<?php
+$hostName = "localhost";
+$userName = "root";
+$password = "12345PRI";
+$databaseName = "pravasi_ngo_db";
+$conn = new mysqli($hostName, $userName, $password, $databaseName);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
